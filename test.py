4d39ce1973e3c40cb1a550d659741495c74762da
KIBORG04 <bossmira4@gmail.com>
2024-11-29 03:42:38 +0300

    Ужосы

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
-test = [
-    [1, 2, 3],
-    [3, 4, 5],
-    [6, 7, 8],
-    [11, 12,13]
-]
+import numpy as np
+import matplotlib.pyplot as plt
 
-print([sub_array[-1] for sub_array in test[2:]])
+# Пример данных (замени на свои)
+time = np.arange(0, 50, 0.01)  # Время от 0 до 50 с шагом 0.01
+concentration = np.random.rand(len(time)) * np.exp(-((time - 25)**2) / (2 * 10**2))  # Пример концентрации
+
+print(time, concentration, sep='\n')
+
+# Связь времени с молекулярной массой (примем линейную зависимость)
+k = 400  # Коэффициент, регулирующий масштаб масс (подбери по данным)
+molecular_weights = time  # Переводим время в молекулярную массу
+
+# Нормируем концентрацию
+concentration /= np.sum(concentration)
+
+# Построение графика ММР
+plt.plot(molecular_weights, concentration)
+plt.title('Результат расчета имитационной модели ММР')
+plt.xlabel('Молекулярная масса')
+plt.ylabel('Концентрация')
+plt.show()
